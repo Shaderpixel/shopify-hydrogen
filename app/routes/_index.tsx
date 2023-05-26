a6e3d45c7be73c4522ac7437646c400f8d339de9
Shaderpixel <lock.j.h@gmail.com>
2023-05-26 13:55:23 +0800

    feat: custom homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,6 @@
+import {Link, useLoaderData} from '@remix-run/react';
+import {Image} from '@shopify/hydrogen';
+
 export function meta() {
   return [
     {title: 'Hydrogen'},
@@ -5,10 +8,62 @@ export function meta() {
   ];
 }
 
+export async function loader({context}: {[index: string]: any}) {
+  // this runs server side
+  return context.storefront.query(COLLECTIONS_QUERY);
+}
+
+// site homepage
 export default function Index() {
+  const {collections} = useLoaderData();
+  console.debug('collections', collections);
+
   return (
-    <div>
-      <h3>Hello from the home page!</h3>
-    </div>
+    <section className="w-full gap-4">
+      <h2 className="text-lead max-w-prose whitespace-pre-wrap font-bold">
+        Collections
+      </h2>
+      <div className="grid grid-flow-row grid-cols-1 gap-2 gap-y-6 sm:grid-cols-3 md:gap-4 lg:gap-6">
+        {collections.nodes.map((collection: any) => {
+          return (
+            <Link to={`/collections/${collection.handle}`} key={collection.id}>
+              <div className="grid gap-4">
+                {collection?.image && (
+                  <Image
+                    alt={`Image of ${collection.title}`}
+                    data={collection.image}
+                    key={collection.id}
+                    sizes="(max-width: 32em) 100vw, 33vw"
+                    widths={[400, 500, 600, 700, 800, 900]}
+                    crop="center"
+                  />
+                )}
+                <h2 className="text-copy max-w-prose whitespace-pre-wrap font-medium">
+                  {collection.title}
+                </h2>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
   );
 }
+
+const COLLECTIONS_QUERY = `#graphql
+	query FeatureCollections {
+		collections (first: 3, query: "collection_type:smart") {
+			nodes {
+				id
+				title
+				handle
+				image {
+          altText
+          width
+          height
+          url
+        }
+			}
+		}
+	}
+`;
