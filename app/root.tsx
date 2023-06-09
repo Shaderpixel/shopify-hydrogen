3c02b109f5a116d3b15a6acd39c6d67e54a91290
Shaderpixel <lock.j.h@gmail.com>
2023-06-09 14:19:39 +0800

    feat: add cart query in root route and access it in cart drawer in Layout component. Optimistic UI changes for price

--- app/root.tsx
+++ app/root.tsx
@@ -8,9 +8,15 @@ import {
 } from '@remix-run/react';
 import {Seo} from '@shopify/hydrogen';
 import type {Shop} from '@shopify/hydrogen/storefront-api-types';
-import {type LinksFunction, type LoaderArgs} from '@shopify/remix-oxygen';
+import {
+  defer,
+  type LinksFunction,
+  type LoaderArgs,
+} from '@shopify/remix-oxygen';
 import {Layout} from 'app/components/Layout';
+import {BadTypeObject} from 'types';
 import favicon from '../public/favicon.svg';
+import {CART_QUERY} from './queries/cart';
 
 import styles from './styles/app.css';
 import tailwind from './styles/tailwind-build.css';
@@ -31,9 +37,32 @@ export const links: LinksFunction = () => {
   ];
 };
 
-export async function loader({context}: LoaderArgs) {
-  const layout = await context.storefront.query<{shop: Shop}>(LAYOUT_QUERY);
-  return {layout};
+async function getCart({storefront}: BadTypeObject, cartId: string) {
+  if (!storefront) {
+    throw new Error('missing storefront client in cart query');
+  }
+
+  const {cart} = await storefront.query(CART_QUERY, {
+    variables: {
+      cartId,
+      country: storefront.i18n.country,
+      language: storefront.i18n.language,
+    },
+    cache: storefront.CacheNone(),
+  });
+
+  return cart;
+}
+
+export async function loader({context, request}: LoaderArgs) {
+  const cartId = await context.session.get('cartId');
+
+  // The defer function can be used to load data asynchronously. It returns a promise that resolves to the data that you pass to it.
+  //In the code block below, layout is loaded synchronously with await, and cart is loaded asynchronously.
+  return defer({
+    cart: cartId ? getCart(context, cartId) : undefined,
+    layout: await context.storefront.query<{shop: Shop}>(LAYOUT_QUERY),
+  });
 }
 
 export default function App() {
